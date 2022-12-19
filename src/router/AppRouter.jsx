c1994c80e53bf7b1a6968b9da3c9061d6a5d5d6d
Camilo Suaza <camilo.suaza12@gmail.com>
2022-12-19 17:15:42 -0500

    adding routes for my app

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,33 @@ import { LoginPage } from "../auth/pages";
 import { HeoresRoutes } from "../heroes";
 import { DcPage, MarvelPage } from "../heroes/pages";
 import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeoresRoutes />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        {/* <Route path="login" element={<LoginPage />} /> */}
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeoresRoutes />
+            </PrivateRoute>
+          }
+        />
+
+        {/* <Route path="/*" element={<HeoresRoutes />} /> */}
       </Routes>
     </>
   );
